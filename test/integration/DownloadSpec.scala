a1b831a83c440d7613a5b70c0e6ca61b789617f1
Manabu Nakamura <gaku_github@t2v.jp>
2015-06-16 01:43:51 +0900

    OneAppPerTest

--- test/integration/DownloadSpec.scala
+++ test/integration/DownloadSpec.scala
@@ -4,24 +4,21 @@ import org.scalatest.FunSpec
 import play.api.test.{FakeRequest, FakeApplication}
 import play.api.test.Helpers._
 import helper.TestHelper._
+import org.scalatestplus.play._
 
-class DownloadSpec extends FunSpec {
+class DownloadSpec extends FunSpec with OneAppPerTest {
 
   describe("#index") {
     it("should contains value") {
-      running(FakeApplication()) {
-        val res = route(FakeRequest(GET, "/download")).get
-        assert(status(res) == 200)
-        assert(contentContains(res, ",name1234,"))
-      }
+      val res = route(FakeRequest(GET, "/download")).get
+      assert(status(res) == 200)
+      assert(contentContains(res, ",name1234,"))
     }
 
     it("should not contains value") {
-      running(FakeApplication()) {
-        val res = route(FakeRequest(GET, "/download")).get
-        assert(status(res) == 200)
-        assert(!contentContains(res, "1234567"))
-      }
+      val res = route(FakeRequest(GET, "/download")).get
+      assert(status(res) == 200)
+      assert(!contentContains(res, "1234567"))
     }
   }
 
